cda31396173208b6e438da5566d3a16d295f086b
jeffer233 <jeffersonthomas67890@gmail.com>
2022-09-08 23:36:03 +0800

    adding

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const { response } = require('express');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'payboxdb'
+    }
+});  
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors()) 
+
+app.get('/', (req, res) => {
+    res.send('succeed');
+})
+
+app.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    db('users')
+     .returning('*')
+     .insert({
+       email: email,
+       password: password 
+    })
+    .then(user => {
+        res.json(user[0]);  
+    })
+    .catch(err => res.status(400).json('unable to register'))
+})
+
+app.listen(3000, ()=> {
+    console.log('app is sweet');
+})
